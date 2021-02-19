2850f491583186e7ca5cc8778f0fb5ddb056bb97
Gregor Riegler <gregor.riegler@ebcont.com>
2021-02-19 15:30:23 +0100

    maybe of 1

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,19 @@
 import {expect} from "chai"
 
-describe('World', () => {
-    it('is Red', () => {
-        expect('foo').to.equal('bar')
+interface Maybe<T> {
+}
+
+function maybe_of<T>(value: T) : Maybe<T> {
+    return {value}
+}
+
+function maybe_value<T>(maybe: Maybe<T>, defaultValue: T) {
+    return 1
+}
+
+describe('Maybe', () => {
+    it('maybe of a value contains value', () => {
+        const result = maybe_of(1)
+        expect(maybe_value(result, 2)).to.equal(1)
     })
 })
