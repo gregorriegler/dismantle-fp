7397a63436eba5742c2099024058f7293a7142e2
Gregor Riegler <gregor.riegler@ebcont.com>
2021-02-19 16:38:12 +0100

    extract maybe module

---
+++ test/maybe.ts
@@ -0,0 +1,44 @@
+const NONE: PrivateMaybe<any> = {value: undefined}
+
+export interface Maybe<T> {
+}
+
+export function maybe_of<T>(value: T): Maybe<T> {
+    return {value}
+
+}
+
+export function maybe_value<T>(maybe: Maybe<T>, defaultValue: T): T {
+    return maybe_fold(maybe, identity, () => defaultValue)
+}
+
+export function maybe_none<T>(): Maybe<T> {
+    return NONE
+}
+
+export function maybe_map<T, U>(maybe: Maybe<T>, f: (a: T) => U): Maybe<U> {
+    return maybe_fold(maybe, (a) => maybe_of(f(a)), maybe_none);
+}
+
+export function maybe_lift<T, U>(f: (a: T) => U): ((a: Maybe<T>) => Maybe<U>) {
+    return (a: Maybe<T>) => maybe_map(a, f)
+}
+
+export function maybe_fold<T, U>(maybe: Maybe<T>, some: (a: T) => U, none: () => U): U {
+    const privateMaybe = maybe as PrivateMaybe<T>;
+
+    if (privateMaybe.value) {
+        return some(privateMaybe.value)
+    } else {
+        // if we are empty we use the initial value
+        return none()
+    }
+}
+
+interface PrivateMaybe<T> extends Maybe<T> {
+    value?: T
+}
+
+function identity<T>(a: T) {
+    return a
+}
--- test/maybe_test.ts
+++ test/maybe_test.ts
@@ -1,48 +1,5 @@
 import { expect } from "chai"
-
-interface Maybe<T> {
-}
-
-interface PrivateMaybe<T> extends Maybe<T> {
-    value?: T
-}
-
-const NONE: PrivateMaybe<any> = {value: undefined}
-
-function maybe_of<T>(value: T): Maybe<T> {
-    return {value}
-}
-
-function fold<T, U>(maybe: Maybe<T>, some: (a: T) => U, none: () => U): U {
-    const privateMaybe = maybe as PrivateMaybe<T>;
-
-    if (privateMaybe.value) {
-        return some(privateMaybe.value)
-    } else {
-        // if we are empty we use the initial value
-        return none()
-    }
-}
-
-function identity<T>(a: T) {
-    return a
-}
-
-function maybe_value<T>(maybe: Maybe<T>, defaultValue: T): T {
-    return fold(maybe, identity, () => defaultValue)
-}
-
-function maybe_none<T>(): Maybe<T> {
-    return NONE
-}
-
-function maybe_map<T, U>(maybe: Maybe<T>, f: (a: T) => U): Maybe<U> {
-    return fold(maybe, (a) => maybe_of(f(a)), maybe_none);
-}
-
-function maybe_lift<T, U>(f: (a: T) => U): ((a: Maybe<T>) => Maybe<U>) {
-    return (a: Maybe<T>) => maybe_map(a, f)
-}
+import { maybe_lift, maybe_map, maybe_none, maybe_of, maybe_value } from "./maybe";
 
 describe('Maybe', () => {
     it('maybe of a value contains value', () => {
