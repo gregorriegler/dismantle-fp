e51017574607689f13b2b1dce343b234883519ed
Gregor Riegler <gregor.riegler@ebcont.com>
2021-03-05 14:03:10 +0100

    remove duplication

--- test/maybe.ts
+++ test/maybe.ts
@@ -11,8 +11,8 @@ export function maybe_value<T>(maybe: Maybe<T>, defaultValue: T): T {
     return maybe_fold(maybe, identity, () => defaultValue)
 }
 
-export function maybe_value_unary<T>(maybe: Maybe<T>): ((a: T) => T) {
-    return a => (maybe as PrivateMaybe<T>).value || a
+export function maybe_value_unary<T>(maybe: Maybe<T>): ((defaultValue: T) => T) {
+    return (defaultValue) => maybe_fold(maybe, identity, () => defaultValue)
 }
 
 export function maybe_none<T>(): Maybe<T> {
@@ -36,12 +36,7 @@ export function maybe_lift<T, U>(f: (a: T) => U): ((a: Maybe<T>) => Maybe<U>) {
 export function maybe_fold<T, U>(maybe: Maybe<T>, some: (a: T) => U, none: () => U): U {
     const privateMaybe = maybe as PrivateMaybe<T>;
 
-    if (privateMaybe.value) {
-        return some(privateMaybe.value)
-    } else {
-        // if we are empty we use the initial value
-        return none()
-    }
+    return privateMaybe.value ? some(privateMaybe.value) : none();
 }
 
 interface PrivateMaybe<T> extends Maybe<T> {
