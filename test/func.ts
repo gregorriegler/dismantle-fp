2bbf8f67d5ca55472c9f3550bad34ee1bf25e487
Peter Kofler <peter.kofler@code-cop.org>
2021-03-05 20:57:37 +0100

    Add custom function types to save typing.

--- test/func.ts
+++ test/func.ts
@@ -2,6 +2,22 @@
  * General support functions to enable FP style programming.
  */
 
+export interface F0<R> {
+    (): R
+}
+
+export interface F1<T, R> {
+    (t: T): R
+}
+
+export interface F2<T, U, R> {
+    (t: T, u: U): R
+}
+
+export interface F3<T, U, V, R> {
+    (t: T, u: U, v: V): R
+}
+
 export function identity1<T>(a: T) {
     return a
 }
@@ -9,35 +25,38 @@ export function identity1<T>(a: T) {
 /**
  * Partially apply a binary function in the first argument.
  */
-export function partial2_1<T, U, R>(f: (t: T, u: U) => R, first: T): (u: U) => R {
+export function partial2_1<T, U, R>(f: F2<T, U, R>, first: T): F1<U, R> {
     return (u: U) => f(first, u)
 }
 
 /**
  * Partially apply a binary function in the second argument.
  */
-export function partial2_2<T, U, R>(f: (t: T, u: U) => R, second: U): (t: T) => R {
+export function partial2_2<T, U, R>(f: F2<T, U, R>, second: U): F1<T, R> {
     return (t: T) => f(t, second)
 }
 
 /**
  * Curry a binary function (which is partially applied in first argument).
  */
-export function curry2<T, U, R>(f: (t: T, u: U) => R): (t: T) => ((u: U) => R) {
+export function curry2<T, U, R>(f: F2<T, U, R>): (t: T) => F1<U, R> {
     return (t: T) => partial2_1(f, t)
 }
 
-export function partial3_1<T, U, V, R>(f: (t: T, u: U, v: V) => R, first: T): (u: U, v: V) => R {
+export function partial3_1<T, U, V, R>(f: F3<T, U, V, R>, first: T): F2<U, V, R> {
     return (u: U, v: V) => f(first, u, v)
 }
 
 /**
  * Curry a ternary function (which is partially applied in first argument twice).
  */
-export function curry3<T, U, V, R>(f: (t: T, u: U, v: V) => R): (t: T) => (u: U) => (v: V) => R {
+export function curry3<T, U, V, R>(f: F3<T, U, V, R>): F1<T, F1<U, F1<V, R>>> {
     return (t: T) => curry2<U, V, R>(partial3_1<T, U, V, R>(f, t))
 }
 
-export function apply1<T, R>(f: (t: T) => R, value: T): R {
+/**
+ * Apply an unary function.
+ */
+export function apply1<T, R>(f: F1<T, R>, value: T): R {
     return f(value)
 }
