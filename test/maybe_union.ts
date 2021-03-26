31c7cdbc60361ab41a4ca5f8609041be2fe2fd1c
Gregor Riegler <gregor.riegler@ebcont.com>
2021-03-26 12:58:50 +0100

    formatting

--- test/maybe_union.ts
+++ test/maybe_union.ts
@@ -1,6 +1,6 @@
-import { compose1, F0, F1, identity1, partial2_1, partial2_2 } from "./func"
+import { compose1, F0, F1, identity1 } from "./func"
 
-const NONE: Maybe<never> = { hint: 'None' }
+const NONE: Maybe<never> = {hint: 'None'}
 
 export type Maybe<T> = Value<T> | None<T>
 
@@ -13,10 +13,6 @@ export interface None<T> {
     readonly hint: 'None'
 }
 
-function maybe_none_supplier() {
-    return () => NONE;
-}
-
 export function maybe_none<T>(): Maybe<T> {
     return NONE
 }
@@ -26,7 +22,7 @@ export function maybe_is_none<T>(value: Maybe<T>): value is None<T> {
 }
 
 export function maybe_of<T>(value: T): Maybe<T> {
-    return { value: value, hint: 'Value' }
+    return {value: value, hint: 'Value'}
 }
 
 export function maybe_f<T, R>(f: F1<T, R>): F1<T, Maybe<R>> {
@@ -41,15 +37,15 @@ export function maybe_map<T, R>(maybe: Maybe<T>, f: F1<T, R>): Maybe<R> {
     return maybe_lift(f)(maybe)
 }
 
-export function maybe_lift<T, R>(f: F1<T, R>): F1<Maybe<T>, Maybe<R>> {
-    return maybe => maybe_is_none(maybe) ? NONE : maybe_of(f(maybe.value))
-}
-
 export function maybe_flat_map<T, R>(maybe: Maybe<T>, f: F1<T, Maybe<R>>): Maybe<R> {
     return maybe_bind(f)(maybe)
 }
 
-export function maybe_bind<T, R>(f: F1<T, Maybe<R>>) : F1<Maybe<T>, Maybe<R>> {
+export function maybe_lift<T, R>(f: F1<T, R>): F1<Maybe<T>, Maybe<R>> {
+    return maybe => maybe_is_none(maybe) ? NONE : maybe_of(f(maybe.value))
+}
+
+export function maybe_bind<T, R>(f: F1<T, Maybe<R>>): F1<Maybe<T>, Maybe<R>> {
     return maybe => maybe_is_none(maybe) ? NONE : f(maybe.value)
 }
 
@@ -57,6 +53,6 @@ export function maybe_fold<T, R>(maybe: Maybe<T>, some: F1<T, R>, none: F0<R>): 
     return maybe_fold1<T, R>(maybe)(some, none)
 }
 
-function maybe_fold1<T,  R>(maybe: Maybe<T>): (some: F1<T, R>, none: F0<R>) => R {
+function maybe_fold1<T, R>(maybe: Maybe<T>): (some: F1<T, R>, none: F0<R>) => R {
     return (some: F1<T, R>, none: F0<R>) => !maybe_is_none(maybe) ? some((maybe as Value<T>).value) : none()
 }
