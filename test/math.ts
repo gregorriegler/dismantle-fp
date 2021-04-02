795ad0739730d95d8c1170f5f97d67ddab819066
Peter Kofler <peter.kofler@code-cop.org>
2021-04-02 18:44:28 +0200

    Create pure supplier experiment as range.

--- test/math.ts
+++ test/math.ts
@@ -1,4 +1,4 @@
-import { F0 } from "./func"
+import { F0, F1 } from "./func"
 import { Maybe, maybe_none, maybe_of } from "./maybe_union"
 
 export function inc(a: number): number {
@@ -16,5 +16,34 @@ export function range_supplier(from: number, to: number): F0<Maybe<number>> {
             return maybe_none()
         }
         return maybe_of(i++)
+    }
+}
+
+export interface SupplyInitial<C, V> {
+    supply: F1<C, SupplyStep<C, V>>,
+    context: C
+}
+
+export interface SupplyStep<C, V> {
+    value: Maybe<V>,
+    context: C
+}
+
+export function range_supplier_pure(from: number, to: number): SupplyInitial<number, number> {
+    const supply: F1<number, SupplyStep<number, number>> = (currentContext: number) => {
+        if (currentContext > to) {
+            return {
+                value: maybe_none(),
+                context: currentContext
+            }
+        }
+        return {
+            value: maybe_of(currentContext),
+            context: currentContext + 1
+        }
+    }
+    return {
+        supply: supply,
+        context: from
     }
 }
--- test/math_test.ts
+++ test/math_test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai"
 import { it } from "mocha"
-import { is_divided, range_supplier } from "./math"
+import { is_divided, range_supplier, range_supplier_pure } from "./math"
 import { expectEmpty, expectValue } from "./maybe_expects"
 
 describe("Math", () => {
@@ -23,6 +23,19 @@ describe("Math", () => {
         expectValue(range(), 2)
         expectValue(range(), 3)
         expectEmpty(range())
+    })
+
+    it("pure range", () => {
+        const { supply: supply, context: initialContext } = range_supplier_pure(1, 3)
+
+        const { value: value1, context: context1 } = supply(initialContext)
+        expectValue(value1, 1)
+        const { value: value2, context: context2 } = supply(context1)
+        expectValue(value2, 2)
+        const { value: value3, context: context3 } = supply(context2)
+        expectValue(value3, 3)
+        const { value: value4 } = supply(context3)
+        expectEmpty(value4)
     })
 
 })
