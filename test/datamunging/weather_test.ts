f945abaf65a87c508a987d43e22f4486fac5a02a
Peter Kofler <peter.kofler@code-cop.org>
2021-06-18 13:47:23 +0200

    Comments and reorder methods.

--- test/datamunging/weather_test.ts
+++ test/datamunging/weather_test.ts
@@ -1,28 +1,6 @@
 import { expect } from "chai"
 import { readFileSync } from "fs"
 import { compose0, F0, F1, identity1 } from "../func"
-import { expectEmpty, expectValue } from "../maybe_expects"
-import { Maybe, maybe_none, maybe_of } from "../maybe_union"
-import { Seq, seq_of_empty, seq_of_supplier } from "../seq"
-import { expect_seq_four_values, expect_seq_three_values } from "../seq_expects"
-
-/*
-Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
-
-   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5
-   2  79    63    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5
-*/
-
-/*
-* Algorithm (solution)
-* - read lines
-* - calc spread
-* - find min
-* - report day
-*
-* Design (units)
-* - outside in
-*/
 
 function io_read_file(fileName: string): F0<string> {
     return () => readFileSync(fileName).toString()
@@ -30,7 +8,7 @@ function io_read_file(fileName: string): F0<string> {
 
 const TestFile = "./test/datamunging/part1/testFile.dat"
 
-describe("Weather Data", () => {
+describe("Weather Data infrastructure", () => {
 
     describe("IO", () => {
         it("io_read_file", () => {
@@ -66,13 +44,23 @@ function reader_apply<IO, R>(reader: Reader<IO, R>, io: F0<IO>): F0<R> {
     return compose0(io, reader.operation)
 }
 
+/*
+Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
 
+   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5
+   2  79    63    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5
+*/
 
-
-
-
-
-
+/*
+* Algorithm (solution)
+* - read lines
+* - calc spread
+* - find min
+* - report day
+*
+* Design (units)
+* - outside in
+*/
 
 const Data1Line = "./test/datamunging/part1/weather1line.dat"
 
@@ -97,7 +85,3 @@ describe("Weather Data", () => {
         //     "  mo  82.9  60.5  71.7    16  58.8       0.00              6.9          5.3")
     })
 })
-
-// function io_read_file(filename: string): () => Seq<string> {
-//     return () => seq_of_supplier(read_file(filename))
-// }
