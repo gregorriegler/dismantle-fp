805e79ce7582adc0f7c7db0fefe32d2ea468a6d6
Peter Kofler <peter.kofler@code-cop.org>
2021-07-02 12:55:13 +0200

    Simplify apply, alias Read.

--- test/datamunging/weather_test.ts
+++ test/datamunging/weather_test.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai"
 import { readFileSync } from "fs"
 import { EOL } from "os"
-import { compose0, compose1, F0, F1, F2, identity1 } from "../func"
+import { compose1, F0, F1, F2, identity1 } from "../func"
 
-function io_read_file(fileName: string): F0<string> {
+function io_read_file(fileName: string): Read<string> {
     return () => readFileSync(fileName).toString()
 }
 
@@ -17,40 +17,38 @@ describe("Reader", () => {
     })
 
     it("executes io", () => {
-        const io_function = io_read_file(TestFile)
         const reader = reader_of<string>()
 
+        const io_function = io_read_file(TestFile)
         const result = reader_apply(reader, io_function)
 
-        expect(result()).to.equal("a" + EOL + "b" + EOL)
+        expect(result).to.equal("a" + EOL + "b" + EOL)
     })
 
     it("maps io", () => {
-        const io_function = io_read_file(TestFile)
         const reader = reader_of<string>()
         const mapped_reader = reader_map(reader, (s) => s.split(EOL))
 
+        const io_function = io_read_file(TestFile)
         const result = reader_apply(mapped_reader, io_function)
 
-        expect(result()).to.deep.equal(["a", "b", ""])
+        expect(result).to.deep.equal(["a", "b", ""])
     })
 
     it("binds io", () => {
-
+        const reader = reader_of<string>()
         function countLines(s: string): Reader<string, number> {
             const reader = reader_of<string>()
             const mapped_reader = reader_map(reader, (s) => s.split(EOL))
             const count_reader = reader_map(mapped_reader, (array) => array.length)
             return count_reader
         }
-
-        const io_function = io_read_file(TestFile)
-        const reader = reader_of<string>()
         const mapped_reader = reader_bind(countLines)(reader)
 
+        const io_function = io_read_file(TestFile)
         const result = reader_apply(mapped_reader, io_function)
 
-        expect(result()).to.equal(3)
+        expect(result).to.equal(3)
     })
 
     // functor needs 2 operations
@@ -58,6 +56,49 @@ describe("Reader", () => {
     // monad needs 2 operations
     // of, flatmap (bind)
 })
+
+type Read<IO> = F0<IO>
+
+interface Input<IO, R> extends Object {
+    readonly transform: F1<IO, R>
+}
+
+export type Reader<IO, R> = Input<IO, R>
+
+export function reader_of<IO>(): Reader<IO, IO> {
+    return { transform: identity1 }
+}
+
+export function reader_map<IO, T, R>(reader: Reader<IO, T>, f: F1<T, R>): Reader<IO, R> {
+    return { transform: (compose1(reader.transform, f)) }
+}
+
+export function reader_lift<IO, T, R>(f: F1<T, R>): F1<Reader<IO, T>, Reader<IO, R>> {
+    return (reader) => reader_map(reader, f)
+}
+
+export function reader_flatmap<IO, T, R>(reader: Reader<IO, T>, f: F1<T, Reader<IO, R>>): Reader<IO, R> {
+    return reader_bind(f)(reader)
+}
+
+export function reader_bind<IO, T, R>(f: F1<T, Reader<IO, R>>): F1<Reader<IO, T>, Reader<IO, R>> {
+    return (reader: Reader<IO, T>) => {
+        function nestedTransform(input: IO) {
+            const transformedInput = reader_apply_single(reader, input)
+            return reader_apply_single(f(transformedInput), input)
+            // see https://gist.github.com/teazaid/c8e200ad07156de22da94c01ffc81014#file-userrepositorywrappermonad-scala
+        }
+        return { transform: nestedTransform }
+    }
+}
+
+export function reader_apply<IO, R>(reader: Reader<IO, R>, ioRead: Read<IO>): R {
+    return reader_apply_single(reader, ioRead())
+}
+
+function reader_apply_single<IO, R>(reader: Reader<IO, R>, input: IO): R {
+    return reader.transform(input)
+}
 
 // TODO 3. make this test work
 // describe("Writer", () => {
@@ -76,47 +117,6 @@ describe("Reader", () => {
 //     })
 // })
 
-interface Input<IO, R> extends Object {
-    readonly transform: F1<IO, R>
-}
-
-export type Reader<IO, R> = Input<IO, R>
-
-export function reader_of<IO>(): Reader<IO, IO> {
-    return {transform: identity1}
-}
-
-export function reader_map<IO, T, R>(reader: Reader<IO, T>, f: F1<T, R>): Reader<IO, R> {
-    return {transform: (compose1(reader.transform, f))}
-}
-
-export function reader_lift<IO, T, R>(f: F1<T, R>): F1<Reader<IO, T>, Reader<IO, R>> {
-    return (reader) => reader_map(reader, f)
-}
-
-export function reader_flatmap<IO, T, R>(reader: Reader<IO, T>, f: F1<T, Reader<IO, R>>): Reader<IO, R> {
-    return reader_bind(f)(reader)
-}
-
-function reader_apply_single<IO, R>(reader: Reader<IO, R>, input: IO): R {
-    return reader.transform(input);
-}
-
-export function reader_bind<IO, T, R>(f: F1<T, Reader<IO, R>>): F1<Reader<IO, T>, Reader<IO, R>> {
-    return (reader: Reader<IO, T>) => {
-        function nestedTransform(input: IO) {
-            const transformedInput: T = reader_apply_single(reader, input);
-            return reader_apply_single(f(transformedInput), input);
-        }
-
-        return {transform: nestedTransform}
-    }
-}
-
-export function reader_apply<IO, R>(reader: Reader<IO, R>, io: F0<IO>): F0<R> {
-    return () => reader_apply_single(reader, io())
-}
-
 // TODO 2. Output only has Type IO
 interface Output<T, IO> extends Object {
     readonly write: F2<T, IO, never>
@@ -125,7 +125,7 @@ interface Output<T, IO> extends Object {
 export type Writer<T, IO> = Output<T, IO>
 
 export function writer_of<T, IO>(write: F2<T, IO, never>): Writer<T, IO> {
-    return {write: write}
+    return { write: write }
 }
 
 export function writer_map<V, T, IO>(writer: Writer<T, IO>, f: F1<V, T>): Writer<V, IO> {
@@ -167,11 +167,11 @@ describe("Weather Data", () => {
     it("find_min_spread", () => {
         const reader: Input<string, string> = reader_of()
 
-        const reader_mapped: Reader<string, number> = reader_map(reader, find_min_spread);
+        const reader_mapped: Reader<string, number> = reader_map(reader, find_min_spread)
 
         const io_function = io_read_file(TestFile)
         const result = reader_apply(reader_mapped, io_function)
-        expect(result()).to.equal(1)
+        expect(result).to.equal(1)
     })
 
     // TODO print final solution to console
