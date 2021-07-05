547fbda5a05d7a423ad6449148118128470f71f0
Peter Kofler <peter.kofler@code-cop.org>
2021-07-05 22:26:54 +0200

    Add code for weather data, needs Seq filter to work.

--- test/datamunging/weather_test.ts
+++ test/datamunging/weather_test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai"
 import { EOL } from "os"
-import { Seq, seq_filter, seq_map, seq_of_array } from "../seq"
+import { Seq, seq_filter, seq_fold, seq_map, seq_of_array } from "../seq"
 import { io_read_file, Reader, reader_apply, reader_map, reader_of } from "./reader"
 import { Writer, writer_apply, writer_map, writer_of } from "./writer"
 
@@ -31,26 +31,43 @@ function splitIntoLines(fileText: string): Seq<string> {
     return seq_of_array(fileText.split(EOL))
 }
 
-function trimLines(lines: Seq<string>): Seq<string> {
-    function trim(line: string): string {
-        return line.trim()
-    }
-    return seq_map(lines, trim)
+function trim(line: string): string {
+    return line.trim()
 }
 
-function filterNonEmptyLines(lines: Seq<string>): Seq<string> {
-    function isNonEmptyLine(line: string): boolean {
-        return line.trim().length > 0
-    }
-    return seq_filter(lines, isNonEmptyLine)
+function isNonEmptyLine(line: string): boolean {
+    return line.length > 0
 }
 
-function filterDataLines(lines: Seq<string>): Seq<string> {
-    function isDataLine(nonEmptyLine: string): boolean {
-        const firstCharacter = nonEmptyLine.trim().charAt(0)
-        return !isNaN(+firstCharacter)
+function isDataLine(nonEmptyLine: string): boolean {
+    const firstCharacter = nonEmptyLine.charAt(0)
+    return !isNaN(+firstCharacter)
+}
+
+interface DataEntry {
+    Dy: number
+    MxT: number
+    MnT: number
+    spread(): number;
+}
+
+function parseData(dataLine: string): DataEntry {
+    const entries = dataLine.split(" ").slice(0, 3).map(parseInt)
+    return {
+        Dy: entries[0],
+        MxT: entries[1],
+        MnT: entries[2],
+        spread() {
+            return this.MxT - this.MnT
+        }
     }
-    return seq_filter(lines, isDataLine)
+}
+
+function minEntry(a: DataEntry, b: DataEntry): DataEntry {
+    if (a.spread() < b.spread()) {
+        return a
+    }
+    return b
 }
 
 // 2nd idea: Go into find_min_spread and map there only once.
@@ -58,7 +75,13 @@ function filterDataLines(lines: Seq<string>): Seq<string> {
 
 function find_min_spread(fileText: string): number {
     // will be calculated using seq
-    return 1
+    const lines = splitIntoLines(fileText)
+    const trimmedLines = seq_map(lines, trim)
+    const nonEmptyLines = seq_filter(trimmedLines, isNonEmptyLine)
+    const dataLines = seq_filter(nonEmptyLines, isDataLine)
+    const dataEntries = seq_map(dataLines, parseData)
+    const min = seq_fold(dataEntries, minEntry, parseData("0 999 0"))
+    return min.Dy
 }
 
 function console_print(message: string) {
@@ -67,7 +90,7 @@ function console_print(message: string) {
 
 describe("Weather Data (application of Reader)", () => {
 
-    it("run application", () => {
+    it("find_min_spread filters", () => {
         const reader: Reader<string, string> = reader_of()
 
         const reader_mapped: Reader<string, number> = reader_map(reader, find_min_spread)
@@ -75,6 +98,16 @@ describe("Weather Data (application of Reader)", () => {
         const io_function = io_read_file(Data1Line)
         const result = reader_apply(reader_mapped, io_function)
         expect(result).to.equal(1)
+    })
+
+    it("run application", () => {
+        const reader: Reader<string, string> = reader_of()
+
+        const reader_mapped: Reader<string, number> = reader_map(reader, find_min_spread)
+
+        const io_function = io_read_file(FullFile)
+        const result = reader_apply(reader_mapped, io_function)
+        // expect(result).to.equal(1)
 
         const writer: Writer<string, string> = writer_of()
         const writer_mapped: Writer<number, string> = writer_map(writer, (n) => "" + n)
