7b3bd303b66f55c0fe07bf47f7fc0c3b83446b9d
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-27 11:36:25 +0200

    tasklist test skeleton

---
+++ test/tasklist/tasklist_test.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai"
+import { describe } from "mocha";
+
+/**
+ * # Phase 1
+ * - create a task with name
+ * - create a task with name
+ * - display list of tasks
+ * - tasks saved as file to disk
+ *
+ * # Phase 2
+ * - update an existing task its name
+ * - remove a task
+ *
+ * # Phase 3
+ * - add additional description, update it
+ */
+describe("TaskList App", () => {
+
+    describe("TaskList Top Level", () => {
+        it("foo", () => {
+            expect(1).to.equal(1)
+        })
+    })
+
+    describe("TaskList Domain", () => {
+
+    })
+})
