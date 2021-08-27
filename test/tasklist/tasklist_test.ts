305d5edf3e04b49b8ce762fe3c069fa9bf77c1e7
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-27 11:51:56 +0200

    task list starts empty

--- test/tasklist/tasklist_test.ts
+++ test/tasklist/tasklist_test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai"
 import { describe } from "mocha";
+import { Writer, writer_apply, writer_of } from "../datamunging/writer";
 
 /**
  * # Phase 1
@@ -37,9 +38,27 @@ describe("TaskList App", () => {
             console.log("Hello")
             expect(output).to.eq("Hello")
         })
+
+        it("starts empty", () => {
+            task_list(["list"])
+            expect(output).to.eq("Current Tasks:\n")
+        })
     })
 
     describe("TaskList Domain", () => {
 
     })
 })
+
+function console_print(message: string) {
+    console.log(message)
+}
+
+function task_list(strings: string[]) {
+    let header = "Current Tasks:\n";
+    let current_tasks = "";
+    let formatted_task_list = header + current_tasks;
+
+    const writer: Writer<string, string> = writer_of()
+    writer_apply(writer, formatted_task_list, console_print)
+}
