1a2a68954c9273788fd52b4e0e1e9408bcb037a8
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-27 12:10:30 +0200

    refactor: unary functions

--- test/tasklist/tasklist_test.ts
+++ test/tasklist/tasklist_test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai"
 import { describe } from "mocha";
 import { Write, Writer, writer_apply, writer_of } from "../datamunging/writer";
+import { curry3 } from "../func";
 
 /**
  * # Phase 1
@@ -50,25 +51,35 @@ describe("TaskList App", () => {
     })
 })
 
-function console_print(message: string) {
-    console.log(message)
-}
+/*
+ * Pure (Domain)
+ */
 
 function format_tasks() {
-    const header = "Current Tasks:\n";
-    const current_tasks = "";
-    const formatted_task_list = header + current_tasks;
-    return formatted_task_list;
+    const header = "Current Tasks:\n"
+    const current_tasks = ""
+    const formatted_task_list = header + current_tasks
+    return formatted_task_list
 }
 
 function formatted_tasks_writer() {
-    const formatted_task_list = format_tasks();
-
-    const writer: Writer<string, string> = writer_of()
-    return (io_write:Write<string>) => { writer_apply(writer, formatted_task_list, io_write) }
+    const formatted_task_list = format_tasks()
+    const string_writer: Writer<string, string> = writer_of()
+    const apply_writer = curry3(writer_apply)
+    const write = apply_writer(string_writer)
+    const write_formatted_task = write(formatted_task_list)
+    return write_formatted_task
 }
 
-function task_list(strings: string[]) {
+/*
+ * Top Level
+ */
+
+function task_list(strings: string[]): void {
     const writer = formatted_tasks_writer()
     writer(console_print)
+}
+
+function console_print(message: string): void {
+    console.log(message)
 }
