5b44c1d42c9d1dfc44a7a8d57457ab4a203312dd
Gregor Riegler <gregor.riegler@ebcont.com>
2021-09-10 10:30:36 +0200

    refactor: function interface to type

--- test/datamunging/writer.ts
+++ test/datamunging/writer.ts
@@ -1,8 +1,13 @@
-import { compose2, F1, identity1 } from "../func"
+import { compose2, curry3, F1, identity1 } from "../func"
 
-export interface Write<IO> {
-    (io: IO): void
-}
+
+/**
+ * A Function that writes our Value (IO) to some output
+ *
+ * @type IO The type of the value that is going to be written
+ * @param io The value that is going to be written
+ */
+export type Write<IO> = (io: IO) => void
 
 interface Output<T, IO> extends Object {
     readonly transform: F1<T, IO>
@@ -18,6 +23,16 @@ export function writer_map<V, T, IO>(writer: Writer<T, IO>, f: F1<V, T>): Writer
     return { transform: (compose2(f, writer.transform)) }
 }
 
+/**
+ *
+ * @param writer
+ * @param t
+ * @param ioWrite the function that takes the Value and outputs it
+ */
 export function writer_apply<T, IO>(writer: Writer<T, IO>, t: T, ioWrite: Write<IO>): void {
     ioWrite(writer.transform(t))
 }
+
+// export function
+// const apply_writer = curry3(writer_apply) // this should be part of the writer lib
+// const write = apply_writer(string_writer) // or even this
