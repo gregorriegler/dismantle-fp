89fa1524ee08922365bd82a8e036ce3bd2e65417
Peter Kofler <peter.kofler@code-cop.org>
2022-01-14 12:13:10 +0100

    Use pair_map in lists_tasks.

--- test/tasklist/use_cases.ts
+++ test/tasklist/use_cases.ts
@@ -10,11 +10,11 @@ import { task_adder, Tasks, tasks_create, tasks_format } from "./tasks"
 type Pair<L_NAME extends string, L, R_NAME extends string, R> = {
     [key in L_NAME]: L
 } & { // & = intersection type
-    [key in R_NAME]: R
-} & { // TODO hide these fields from outside
-    _l_name: L_NAME
-    _r_name: R_NAME
-}
+        [key in R_NAME]: R
+    } & { // TODO hide these fields from outside
+        _l_name: L_NAME
+        _r_name: R_NAME
+    }
 
 export function pair_of<L_NAME extends string, L, R_NAME extends string, R>(
     left_name: L_NAME,
@@ -30,7 +30,7 @@ export function pair_of<L_NAME extends string, L, R_NAME extends string, R>(
     } as Pair<L_NAME, L, R_NAME, R>
 }
 
-function pair_map_left<L_NAME extends string, L, NEW_L, R_NAME extends string, R>(
+function pair_map_l2left<L_NAME extends string, L, NEW_L, R_NAME extends string, R>(
     pair: Pair<L_NAME, L, R_NAME, R>,
     map: (l: L) => NEW_L
 ) {
@@ -42,7 +42,7 @@ function pair_map_left<L_NAME extends string, L, NEW_L, R_NAME extends string, R
     )
 }
 
-function pair_map_right<L_NAME extends string, L, R_NAME extends string, R, NEW_R>(
+function pair_map_r2right<L_NAME extends string, L, R_NAME extends string, R, NEW_R>(
     pair: Pair<L_NAME, L, R_NAME, R>,
     map: (r: R) => NEW_R
 ) {
@@ -54,32 +54,41 @@ function pair_map_right<L_NAME extends string, L, R_NAME extends string, R, NEW_
     )
 }
 
+function pair_map_l2right<L_NAME extends string, L, R_NAME extends string, R, NEW_R>(
+    pair: Pair<L_NAME, L, R_NAME, R>,
+    map: (l: L) => NEW_R
+) {
+    return pair_of(
+        pair._l_name,
+        pair[pair._l_name],
+        pair._r_name,
+        map(pair[pair._l_name])
+    )
+}
+
 export type ApplicationState = Pair<'tasks', Tasks, 'write', WriteApplied<string>>
 
 export function application_state_create(): ApplicationState {
     return pair_of(
         'tasks', tasks_create(),
         'write', null_write
-    );
+    )
 }
 
 export function add_task(state: ApplicationState, task_name: string): ApplicationState {
-    const added_task = pair_map_left(state, task_adder(task_name));
-    return pair_map_right(added_task, _ => {
-        return null_write;
-    })
+    const added_task = pair_map_l2left(state, task_adder(task_name))
+    return pair_map_r2right(added_task, _ => null_write)
 }
 
 export function list_tasks(state: ApplicationState, _: string): ApplicationState {
+    return pair_map_l2right(state, tasks_writer)
+}
+
+function tasks_writer(tasks: Tasks): WriteApplied<string> {
+    const formatted_task_list = tasks_format(tasks)
     const apply_formatted_tasks_writer = create_apply_writer_for_transformation(identity1) // TODO I think this is what new_writer should be doing
-
-    const formatted_task_list = tasks_format(state.tasks)
     const write_formatted_tasks = apply_formatted_tasks_writer(formatted_task_list)
-
-    return pair_of(
-        'tasks', state.tasks,
-        'write', write_formatted_tasks
-    );
+    return write_formatted_tasks
 }
 
 export function write_invalid_command(state: ApplicationState, command_name: string): ApplicationState {
@@ -92,7 +101,7 @@ export function write_invalid_command(state: ApplicationState, command_name: str
     return pair_of(
         'tasks', state.tasks,
         'write', write_invalid_command
-    );
+    )
 }
 
 function format_invalid_command_name(command_name: string) {
