d764b107495298b7c74eb05c70d6b2f8adab17e6
Peter Kofler <peter.kofler@code-cop.org>
2022-02-04 14:09:56 +0100

    Make more generics because we can.

--- test/tasklist/commands.ts
+++ test/tasklist/commands.ts
@@ -10,7 +10,7 @@ import {
     list_tasks,
     write_invalid_command
 } from "./use_cases"
-import { pair_map } from "./pair"
+import { Pair, pair_map } from "./pair"
 
 /*
  * Gibt es einen pure Teil der nicht im Domain ist, wie zB convert von String auf Command.
@@ -21,15 +21,19 @@ import { pair_map } from "./pair"
  * Dann folden wir den State ueber die Commands. Am Ende wandert der State in einen Writer zum Save.
  */
 
+type CommandX<K1, V1, K2, V2, K3, V3, K4, V4> = (state: Pair<K1, V1, K2, WriteApplied<V2>>) => Pair<K3, V3, K4, WriteApplied<V4>>
 type Command = (state: ApplicationState) => ApplicationState
 
 export function task_list(command_names: Seq<UserInput>): WriteApplied<string> {
     const commands = seq_map(command_names, command_from_input)
     const state = seq_fold(commands, commands_fold, application_state_create())
-    return state.write
+    return state.write // this is the only time we use the K2, don't use K1 at all.
 }
 
-function commands_fold(current_state: ApplicationState, command: Command): ApplicationState {
+// function commands_fold(current_state: ApplicationState, command: Command): ApplicationState {
+function commands_fold<K1, V1, K2, V2, K3, V3, K4>(current_state: Pair<K1, V1, "write", WriteApplied<V2>>,
+        command: CommandX<K1, V1, K2, V2, K3, V3, "write", V2>):
+        Pair<K3, V3, K4, WriteApplied<V2>> {
     const new_state = command(current_state)
     const merged_write = write_in_sequence(current_state.write, new_state.write)
     return pair_map(new_state, identity1, lazy(merged_write))
