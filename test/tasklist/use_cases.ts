041d8368f64e9ef615b7c9c7c3119d35bcc21cac
Peter Kofler <peter.kofler@code-cop.org>
2022-02-04 13:53:27 +0100

    Make types most generic. We do not like it.

--- test/tasklist/use_cases.ts
+++ test/tasklist/use_cases.ts
@@ -1,6 +1,6 @@
 import { null_write, WriteApplied } from "../datamunging/write"
 import { create_apply_writer_for_transformation } from "../datamunging/writer"
-import { F1, identity1, lazy } from "../func"
+import { F0, F1, F2, identity1, lazy } from "../func"
 import { Pair, pair_map, pair_of } from "./pair"
 import { task_adder, Tasks, tasks_create, tasks_format } from "./tasks"
 
@@ -17,11 +17,15 @@ export function application_state_create(): ApplicationState {
     )
 }
 
-export function add_task(state: ApplicationState, task_name: string): ApplicationState {
+// export function add_task(state: ApplicationState, task_name: string): ApplicationState {
+export function add_task<K1, K2, V2>(state: Pair<K1, Tasks, K2, WriteApplied<V2>>, task_name: string):
+        Pair<K1, Tasks, K2, WriteApplied<V2>> {
     return pair_map(state, task_adder(task_name), lazy(null_write))
 }
 
-export function list_tasks(state: ApplicationState, _: string): ApplicationState {
+// export function list_tasks(state: ApplicationState, _: string): ApplicationState {
+export function list_tasks<K1, K2, V2>(state: Pair<K1, Tasks, K2, WriteApplied<V2>>, _: string):
+        Pair<K1, Tasks, K2, WriteApplied<string>> {
     return pair_map(state, identity1, tasks_writer)
 }
 
@@ -32,12 +36,16 @@ function tasks_writer(tasks: Tasks): WriteApplied<string> {
     return write_formatted_tasks
 }
 
-export function write_invalid_command(state: ApplicationState, command_name: string): ApplicationState {
+// export function write_invalid_command(state: ApplicationState, command_name: string): ApplicationState {
+export function write_invalid_command<K1, V1, K2>(state: Pair<K1, V1, K2, WriteApplied<string>>, command_name: string):
+        Pair<K1, V1, K2, WriteApplied<string>> {
     return pair_map(state, identity1, create_map_writer_to_invalid_command_writer_for(command_name))
 }
 
-function create_map_writer_to_invalid_command_writer_for(command_name: string) : F1<WriteApplied<string>, WriteApplied<string>> {
-    function map_writer_to_invalid_command_writer(_: WriteApplied<string>) {
+//function create_map_writer_to_invalid_command_writer_for<V1, V2>(command_name: string): F2<V1, V2, WriteApplied<string>> {
+//    function map_writer_to_invalid_command_writer(v1: V1, v2: V2) {
+function create_map_writer_to_invalid_command_writer_for(command_name: string): F0<WriteApplied<string>> {
+    function map_writer_to_invalid_command_writer() {
         const identity = identity1 as F1<string, string>
         const apply_invalid_command_writer = create_apply_writer_for_transformation(identity)
 
