d4b10747d19743f839267f940c012cce808ef428
Peter Kofler <peter.kofler@code-cop.org>
2022-02-25 11:46:37 +0100

    Add main method to run from console.

--- test/tasklist/app.ts
+++ test/tasklist/app.ts
@@ -2,14 +2,22 @@ import { seq_of_array } from "../seq"
 import { task_list, UserInput } from "./commands"
 
 /*
- * Top Level
+ * Top Level, enthält impure Code (Writer).
  */
 export function task_list_app(args: UserInput[]): void {
-    const commands = seq_of_array(args)
-    const writer = task_list(commands)
+    const command_input = seq_of_array(args)
+    const writer = task_list(command_input)
     writer(io_console_print)
 }
 
 function io_console_print(message: string): void {
     console.log(message)
+}
+
+function main() {
+    task_list_app(process.argv.slice(2));
+}
+
+if (require.main === module) {
+    main();
 }
