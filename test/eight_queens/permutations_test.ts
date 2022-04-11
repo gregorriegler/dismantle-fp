9628c9a5087513b2d59431cfd06c7b383252aa97
Peter Kofler <peter.kofler@code-cop.org>
2022-04-11 22:32:23 +0200

    Retrofit array functions.

--- test/eight_queens/permutations_test.ts
+++ test/eight_queens/permutations_test.ts
@@ -1,15 +1,34 @@
 import { expect } from "chai"
 
-function permutations<T>(items: T[]): T[][] {
+// retrofit array
+
+function array_map<T, R>(items: T[], mapFn: (value: T, index: number) => R): R[] {
+    return items.map(mapFn)
+}
+
+function array_flatmap<T, R>(items: T[], mapFn: (value: T, index: number) => R[]): R[] {
+    return array_flatten(array_map(items, mapFn))
+}
+
+function array_flatten<T>(items: T[][]): T[] {
+    return items.reduce((acc, val) => acc.concat(val), [])
+}
+
+export function permutations<T>(items: T[]): T[][] {
     if (items.length == 1) {
         return [items]
     }
-    return items.map((value: T, index: number) => {
+    return array_flatmap(items, (value: T, index: number) => {
         const remainingItems = [...items]
         remainingItems.splice(index, 1)
-        const furtherPermutations = permutations(remainingItems)
-        return furtherPermutations.map((permutation) => [value].concat(permutation));
-    }).reduce((acc, val) => acc.concat(val), []) // flat
+        return foo(value, remainingItems)
+    })
+}
+
+
+function foo<T>(value: T, remainingItems: T[]): T[][] {
+    const furtherPermutations = permutations(remainingItems)
+    return array_map(furtherPermutations, (permutation) => [value].concat(permutation));
 }
 
 describe("permutations", () => {
