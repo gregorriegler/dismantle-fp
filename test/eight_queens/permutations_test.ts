04fcb204efbcafcc00ad3b74a85927ad71ac5dc1
Peter Kofler <peter.kofler@code-cop.org>
2022-04-19 21:06:28 +0200

    seq permutations for [1].

--- test/eight_queens/permutations_test.ts
+++ test/eight_queens/permutations_test.ts
@@ -1,4 +1,8 @@
 import { expect } from "chai"
+import { F0, should_not_call0 } from "../func"
+import { maybe_value } from "../maybe_union"
+import { Seq, seq_first, seq_of_singleton } from "../seq"
+import { expect_seq_empty, expect_seq_n_values } from "../seq_expects"
 
 // --- retrofit array
 
@@ -72,3 +76,39 @@ describe("array permutations", () => {
         expect(p).to.deep.equal([[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]])
     })
 })
+
+// --- seq permutations
+
+export function seq_permutations<T>(items: Seq<T>): Seq<Seq<T>> {
+    return seq_of_singleton(items)
+    // if (items.length == 1) {
+    //     return [items]
+    // }
+    // function permutationsWithout(value: T, index: number): T[][] {
+    //     const removeCurrentItem = array_remover(index)
+    //     const remainingItems = removeCurrentItem(items)
+    //     const furtherPermutations = array_permutations(remainingItems)
+    //     const prepender = array_prepender(value)
+    //     return array_lift(prepender)(furtherPermutations)
+    // }
+    // return array_bind(permutationsWithout)(items)
+}
+
+export function expect_seq_seq_n_values<T>(seq: Seq<Seq<T>>, values: T[][]) {
+    const { head: first, tail } = seq_first(seq)
+    if (values.length == 0) {
+        expect_seq_empty(seq)
+    } else {
+        const first_seq: Seq<T> = maybe_value(first, should_not_call0 as F0<Seq<T>>)
+        expect_seq_n_values(first_seq, ...values[0])
+        expect_seq_seq_n_values(tail, values.slice(1))
+    }
+}
+
+describe("seq permutations", () => {
+    it("of [1]", () => {
+        const seq = seq_permutations(seq_of_singleton(1))
+        expect_seq_seq_n_values(seq, [[1]])
+    })
+})
+
