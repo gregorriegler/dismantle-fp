f17447b633fced7ff5875834c7c02a0cc42ba3ec
Peter Kofler <peter.kofler@code-cop.org>
2022-04-19 20:46:59 +0200

    Permutations tested for 3 elements, assume all dimentions working.

--- test/eight_queens/permutations_test.ts
+++ test/eight_queens/permutations_test.ts
@@ -2,27 +2,27 @@ import { expect } from "chai"
 
 // retrofit array
 
-function array_lift<T, R>(mapFn: (value: T, index: number) => R): (items: T[]) => R[] {
+export function array_lift<T, R>(mapFn: (value: T, index: number) => R): (items: T[]) => R[] {
     return (items) => items.map(mapFn)
 }
 
-function array_map<T, R>(items: T[], mapFn: (value: T, index: number) => R): R[] {
+export function array_map<T, R>(items: T[], mapFn: (value: T, index: number) => R): R[] {
     return array_lift(mapFn)(items)
 }
 
-function array_bind<T, R>(mapFn: (value: T, index: number) => R[]): (items: T[]) => R[] {
+export function array_bind<T, R>(mapFn: (value: T, index: number) => R[]): (items: T[]) => R[] {
     return (items) => array_flatten(array_map(items, mapFn))
 }
 
-function array_flatmap<T, R>(items: T[], mapFn: (value: T, index: number) => R[]): R[] {
+export function array_flatmap<T, R>(items: T[], mapFn: (value: T, index: number) => R[]): R[] {
     return array_bind(mapFn)(items)
 }
 
-function array_flatten<T>(items: T[][]): T[] {
+export function array_flatten<T>(items: T[][]): T[] {
     return items.reduce((acc, val) => acc.concat(val), [])
 }
 
-function array_remover(index: number): <T>(items: T[]) => T[] {
+export function array_remover(index: number): <T>(items: T[]) => T[] {
     return <T>(items: T[]) => {
         const remainingItems = [...items]
         remainingItems.splice(index, 1)
@@ -30,15 +30,15 @@ function array_remover(index: number): <T>(items: T[]) => T[] {
     }
 }
 
-function array_remove<T>(items: T[], index: number): T[] {
+export function array_remove<T>(items: T[], index: number): T[] {
     return array_remover(index)(items)
 }
 
-function array_prepender<T>(value: T): (items: T[]) => T[] {
+export function array_prepender<T>(value: T): (items: T[]) => T[] {
     return (items) => [value].concat(items)
 }
 
-function array_prepend<T>(items: T[], value: T): T[] {
+export function array_prepend<T>(items: T[], value: T): T[] {
     return array_prepender(value)(items)
 }
 
@@ -64,5 +64,9 @@ describe("permutations", () => {
     it("of [1,2]", () => {
         const p = permutations([1, 2])
         expect(p).to.deep.equal([[1, 2], [2, 1]])
+    })
+    it("of [1,2,3]", () => {
+        const p = permutations([1, 2, 3])
+        expect(p).to.deep.equal([[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]])
     })
 })
