e9bbc93d88a6854a37917be693afd3732a2741e5
Peter Kofler <peter.kofler@code-cop.org>
2022-04-27 21:49:03 +0200

    Recurse into all queens. But uses recursion?

--- test/eight_queens/first_test.ts
+++ test/eight_queens/first_test.ts
@@ -15,23 +15,27 @@ import { Seq, seq_filter, seq_map, seq_fold, seq_first, seq_of_array, seq_to_ind
 function are_queens_valid(queens: Seq<number>): boolean {
     const first = seq_first(queens)
     const first_queen_position = first.head
+    function x(): boolean {
 
-    function not_in_lower_diagonal(first_position: number) {
+        function not_in_lower_diagonal(first_position: number) {
 
-        function is_in_lower_diagonal({ index, value: position }: Indexed<number>): boolean {
-            return position == first_position + index || position == first_position - index
+            function is_in_lower_diagonal({ index, value: position }: Indexed<number>): boolean {
+                return position == first_position + index || position == first_position - index
+            }
+
+            const remaining_positions = seq_to_indexed(first.tail, 1)
+            const positions_in_lower_diagonal = seq_filter(remaining_positions, is_in_lower_diagonal)
+            const first_found = seq_first(positions_in_lower_diagonal)
+            const has_any_lower_diagonal = maybe_map(first_found.head, (_) => true)
+            return !maybe_value(has_any_lower_diagonal, lazy(false))
         }
 
-        const remaining_positions = seq_to_indexed(first.tail, 1)
-        const positions_in_lower_diagonal = seq_filter(remaining_positions, is_in_lower_diagonal)
-        const first_found = seq_first(positions_in_lower_diagonal)
-        const has_any_lower_diagonal = maybe_map(first_found.head, (_) => true)
-        return !maybe_value(has_any_lower_diagonal, lazy(false))
+        const lower_diagonal_valid = maybe_map(first_queen_position, not_in_lower_diagonal)
+        return maybe_value(lower_diagonal_valid, lazy(true))
     }
 
-    const lower_diagonal_valid = maybe_map(first_queen_position, not_in_lower_diagonal)
-    return maybe_value(lower_diagonal_valid, lazy(true))
-
+    const xx = maybe_map(first_queen_position, () => x() && are_queens_valid(first.tail))
+    return maybe_value(xx, lazy(true))
     // const has_no_lower_diagonals = seq_map(queens, not_in_lower_diagonal)
     // const lower_diagonal_valid = seq_fold(has_no_lower_diagonals, (a, b) => a && b, true)
     // return lower_diagonal_valid
