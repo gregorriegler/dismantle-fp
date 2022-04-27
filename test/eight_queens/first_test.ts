59b78c86c339a0a25fb8b9d7e8ab30440ff8a6ab
Peter Kofler <peter.kofler@code-cop.org>
2022-04-27 21:43:39 +0200

    Red: find lower diagonal in dimension 3

--- test/eight_queens/first_test.ts
+++ test/eight_queens/first_test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai"
 import { lazy } from "../func"
 import { maybe_map, maybe_value } from "../maybe_union"
-import { Seq, seq_filter, seq_first, seq_of_array, seq_to_indexed, Indexed } from "../seq"
+import { Seq, seq_filter, seq_map, seq_fold, seq_first, seq_of_array, seq_to_indexed, Indexed } from "../seq"
 
 // You must put eight chess queens on an 8×8 chessboard
 // such that none of them is able to capture any other
@@ -12,7 +12,7 @@ import { Seq, seq_filter, seq_first, seq_of_array, seq_to_indexed, Indexed } fro
  2. Which ones are not ok?
  */
 
-function filter_valid(queens: Seq<number>): boolean {
+function are_queens_valid(queens: Seq<number>): boolean {
     const first = seq_first(queens)
     const first_queen_position = first.head
 
@@ -31,32 +31,45 @@ function filter_valid(queens: Seq<number>): boolean {
 
     const lower_diagonal_valid = maybe_map(first_queen_position, not_in_lower_diagonal)
     return maybe_value(lower_diagonal_valid, lazy(true))
+
+    // const has_no_lower_diagonals = seq_map(queens, not_in_lower_diagonal)
+    // const lower_diagonal_valid = seq_fold(has_no_lower_diagonals, (a, b) => a && b, true)
+    // return lower_diagonal_valid
 }
 
-describe("Eight Queens", () => {
-    describe("by filtering", () => {
+describe("Eight Queens filtering", () => {
+    describe("filtering from first element", () => {
 
         it("checks a sequence of dimension 1", () => {
             const queens = seq_of_array([1])
-            const valid = filter_valid(queens)
+            const valid = are_queens_valid(queens)
             expect(valid).to.equal(true)
         })
 
         it("find lower diagonal in dimension 2", () => {
             const queens = seq_of_array([1, 2])
-            const valid = filter_valid(queens)
+            const valid = are_queens_valid(queens)
             expect(valid).to.equal(false)
         })
 
         it("find lower diagonal in dimension 3", () => {
             const queens = seq_of_array([1, 99, 3])
-            const valid = filter_valid(queens)
+            const valid = are_queens_valid(queens)
             expect(valid).to.equal(false)
         })
 
         it("find upper diagonal in dimension 2", () => {
             const queens = seq_of_array([2, 1])
-            const valid = filter_valid(queens)
+            const valid = are_queens_valid(queens)
+            expect(valid).to.equal(false)
+        })
+
+    })
+
+    describe("filtering from second element", () => {
+        it("find lower diagonal in dimension 3", () => {
+            const queens = seq_of_array([99, 1, 2])
+            const valid = are_queens_valid(queens)
             expect(valid).to.equal(false)
         })
 
