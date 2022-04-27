d266755d473bcde8a5655dc24d6ce4738aa4bc52
Peter Kofler <peter.kofler@code-cop.org>
2022-04-27 21:12:47 +0200

    CHanged to use filter.

--- test/eight_queens/first_test.ts
+++ test/eight_queens/first_test.ts
@@ -1,8 +1,7 @@
 import { expect } from "chai"
 import { lazy } from "../func"
 import { maybe_map, maybe_value } from "../maybe_union"
-import { Seq, seq_first, seq_of_array } from "../seq"
-import { expect_seq_n_values } from "../seq_expects"
+import { Seq, seq_filter, seq_first, seq_of_array } from "../seq"
 
 // You must put eight chess queens on an 8×8 chessboard
 // such that none of them is able to capture any other
@@ -16,14 +15,17 @@ import { expect_seq_n_values } from "../seq_expects"
 function filter_valid(queens: Seq<number>): boolean {
     const first = seq_first(queens)
     const first_queen_position = first.head
-    const mh = maybe_map(first_queen_position, (position) => valid_diagonal(first.tail, position + 1))
-    return maybe_value(mh, lazy(true))
-}
 
-function valid_diagonal(seq: Seq<number>, column: number): boolean {
-    const next = seq_first(seq)
-    const next_queen_position = next.head
-    const mh = maybe_map(next_queen_position, (position) => position != column)
+    const mh = maybe_map(first_queen_position, (initial_position) => {
+        const s2 = seq_filter(first.tail, (qzueen_position) => {
+            initial_position += 1
+            return qzueen_position == initial_position
+        })
+        const found = seq_first(s2)
+        const m2 = maybe_map(found.head, (_) => true)
+
+        return ! maybe_value(m2, lazy(false))
+    })
     return maybe_value(mh, lazy(true))
 }
 
@@ -36,12 +38,17 @@ describe("Eight Queens", () => {
             expect(valid).to.equal(true)
         })
 
-        it("find lower diagonal", () => {
+        it("find lower diagonal 2", () => {
             const queens = seq_of_array([1, 2])
+            const valid = filter_valid(queens)
+            expect(valid).to.equal(false)
+        })
+
+        it("find lower diagonal 3", () => {
+            const queens = seq_of_array([1, 99, 3])
             const valid = filter_valid(queens)
             expect(valid).to.equal(false)
         })
 
     })
 })
-
