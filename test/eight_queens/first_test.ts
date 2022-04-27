5e10208e01f5f8db5f5cc0a9d0def2bae8b7e764
Peter Kofler <peter.kofler@code-cop.org>
2022-04-27 20:59:04 +0200

    Find invalid on two, but why?

--- test/eight_queens/first_test.ts
+++ test/eight_queens/first_test.ts
@@ -1,5 +1,7 @@
 import { expect } from "chai"
-import { Seq, seq_of_array, seq_of_singleton } from "../seq"
+import { lazy } from "../func"
+import { maybe_map, maybe_value } from "../maybe_union"
+import { Seq, seq_first, seq_of_array } from "../seq"
 import { expect_seq_n_values } from "../seq_expects"
 
 // You must put eight chess queens on an 8×8 chessboard
@@ -12,7 +14,17 @@ import { expect_seq_n_values } from "../seq_expects"
  */
 
 function filter_valid(queens: Seq<number>): boolean {
-    return true
+    const first = seq_first(queens)
+    const first_queen_position = first.head
+    const mh = maybe_map(first_queen_position, (position) => valid_diagonal(first.tail, position + 1))
+    return maybe_value(mh, lazy(true))
+}
+
+function valid_diagonal(seq: Seq<number>, column: number): boolean {
+    const next = seq_first(seq)
+    const next_queen_position = next.head
+    const mh = maybe_map(next_queen_position, (position) => position != column)
+    return maybe_value(mh, lazy(true))
 }
 
 describe("Eight Queens", () => {
@@ -22,6 +34,12 @@ describe("Eight Queens", () => {
             const queens = seq_of_array([1])
             const valid = filter_valid(queens)
             expect(valid).to.equal(true)
+        })
+
+        it("find lower diagonal", () => {
+            const queens = seq_of_array([1, 2])
+            const valid = filter_valid(queens)
+            expect(valid).to.equal(false)
         })
 
     })
