dd8410fc2568d75b6bf34eefbe334773be6564fc
Peter Kofler <peter.kofler@code-cop.org>
2022-04-27 20:47:39 +0200

    Queens filter list of 1.

--- test/eight_queens/first_test.ts
+++ test/eight_queens/first_test.ts
@@ -1,58 +1,29 @@
 import { expect } from "chai"
-import { Seq, seq_of_singleton } from "../seq"
+import { Seq, seq_of_array, seq_of_singleton } from "../seq"
 import { expect_seq_n_values } from "../seq_expects"
 
 // You must put eight chess queens on an 8×8 chessboard
 // such that none of them is able to capture any other
-// using the standard chess queen?s moves.
+// using the standard chess queen's moves.
 
 /*
- Decomposition:
- dimension 1: 1 solution
- dimension 2: 0 solution
- dimension 3: 0 solution
- dimension 4: 0 solution
- dimension 5: x.... etc.
-              ...x.
-              .x...
-              ....x
-              ..x..
-
- Brute force
- 8^8 = 16.777.216 possible boards  = arrays of 8 numbers 1..8
+ 1. Create permutations -> all permutations of 8 queens.
+ 2. Which ones are not ok?
  */
 
-type Positions = 1 | 2
-
-type BoardPart<K extends Positions> = {
-    [K: string]: Positions
-}
-type Board<P extends Positions> =
-    P extends 1 ? BoardPart<1> :
-    P extends 2 ? BoardPart<1> & BoardPart<2> :
-    never
-
-function all_boards<SIZE extends Positions>(size: SIZE): Seq<Board<SIZE>> {
-    if (size === 1) {
-        return seq_of_singleton({ "1": 1 })
-    }
-    return seq_of_singleton({ "1": 1 })
+function filter_valid(queens: Seq<number>): boolean {
+    return true
 }
 
 describe("Eight Queens", () => {
-    describe("by brute force", () => {
+    describe("by filtering", () => {
 
-        it("produces all combinations size 1", () => {
-            const all_combinations = all_boards(1)
-            expect_seq_n_values(all_combinations, { "1": 1 })
-        })
-
-        xit("produces all combinations size 2", () => {
-            const all_combinations = all_boards(2)
-            expect_seq_n_values(all_combinations,
-                { "1": 1, "2": 2 },
-                { "1": 2, "2": 1 })
+        it("checks a sequence of 1", () => {
+            const queens = seq_of_array([1])
+            const valid = filter_valid(queens)
+            expect(valid).to.equal(true)
         })
 
     })
 })
+
